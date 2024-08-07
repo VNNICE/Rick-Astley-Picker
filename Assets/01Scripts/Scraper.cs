bd777c7e2db025f9cf0ba8f07a8077916d63193e
VNNICE <165227660+VNNICE@users.noreply.github.com>
2024-08-07 16:14:53 +0900

    20240807

--- Assets/01Scripts/Scraper.cs
+++ Assets/01Scripts/Scraper.cs
@@ -5,27 +5,174 @@ using HtmlAgilityPack;
 using UnityEngine.Networking;
 using TMPro;
 using System.Linq;
+using System.Web;
 
 public class Scraper : MonoBehaviour
 {
+    [SerializeField] GameObject scrollViewContent;
+    [SerializeField] GameObject middleContent;
+    [SerializeField] GameObject songTextPrefab;
+    [SerializeField] GameObject selectedSongSpace;
+    [SerializeField] GameObject pauseUI;
     private string url = "https://en.wikipedia.org/wiki/Category:Rick_Astley_songs";
-    [SerializeField] TextMeshProUGUI text;
-    
+    List<string> allSongsList = new List<string>();
+    List<string> shuffledList;
+    bool isDataReady;
+    bool isBindingComplete;
 
     void Awake()
     {
+        pauseUI.SetActive(false);
         StartCoroutine(GetWikipediaData(url));
     }
     private void Start()
     {
-        List<string> shuffledList = ShuffledList<string>(allSongs);
-        foreach (var song in shuffledList) 
+        StartCoroutine(MakeASongsList());
+    }
+    bool shuffleStart;
+    float shuffleTime = 0;
+    bool isPause;
+    private void Update()
+    {
+        if (shuffleStart)
         {
-            text.text += song + "\n";
+            Debug.Log(shuffleTime);
+            shuffleTime += Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+            PauseFunction();;
+        }
+        if (isPause && Input.GetKeyDown(KeyCode.Return))
+        {
+            Application.Quit();
+        }
+    }
+    void PauseFunction() 
+    {
+        if (!isPause)
+        {
+            ShowMenu();
+            Time.timeScale = 0;
+            isPause = true;
+
+        }
+        else if (isPause) 
+        {
+            HideMenu();
+            Time.timeScale = 1;
+            isPause = false;
         }
         
+        void ShowMenu()
+        {
+            pauseUI.SetActive(true);
+        }
+        void HideMenu() 
+        {
+            pauseUI.SetActive(false);
+        }
     }
 
+
+    public void ShuffleStart() 
+    {
+        StartCoroutine(ShuffleEffect());
+    }
+
+    IEnumerator ShuffleEffect() 
+    {
+        string selectedSong = "";
+        shuffleStart = true;
+        Debug.Log("Shuffle Start");
+        while (shuffleTime <= 5f) 
+        {
+            MakeShuffledList();
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        while (shuffleTime <= 10f) 
+        {
+            SelectSong(selectedSong);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        shuffleTime = 0;
+        shuffleStart = false;
+    }
+
+    private IEnumerator MakeASongsList()
+    {
+        Debug.Log("Waiting...");
+        yield return new WaitUntil(() => isDataReady);
+        Debug.Log("Start Make A Songs List");
+        if (isDataReady && !isBindingComplete)
+        {
+            foreach (var gettingSong in allSongsList)
+            {
+                var song = Instantiate(songTextPrefab, scrollViewContent.transform);
+                song.name = gettingSong;
+                var component = song.GetComponent<TextMeshProUGUI>();
+                component.text = gettingSong;
+            }
+            isBindingComplete = true;
+        }
+        StopCoroutine(MakeASongsList());
+        Debug.Log("Create Finished");
+    }
+    void SelectSong(string selectedSong)
+    {
+        int random = Random.Range(0, 10);
+        DestroyChild(selectedSongSpace);
+        selectedSong = shuffledList[random];
+
+        var song = Instantiate(songTextPrefab, selectedSongSpace.transform);
+        song.name = selectedSong;
+        var component = song.GetComponent<TextMeshProUGUI>();
+        component.text = selectedSong;
+    }
+
+    public void MakeShuffledList() 
+    {
+        if (!shuffleStart) 
+        {
+            return;
+        }
+        DestroyChild(middleContent);
+        Shuffle(shuffledList);
+        for (int i = 0; i < 10; i++)
+        {
+            var song = Instantiate(songTextPrefab, middleContent.transform);
+            song.name = shuffledList[i];
+            var component = song.GetComponent<TextMeshProUGUI>();
+            component.text = shuffledList[i];
+        }
+    }
+    void DestroyChild(GameObject parent)
+    {
+        if (parent.transform.childCount > 0)
+        {
+            int childCount = parent.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                Destroy(parent.transform.GetChild(i).gameObject);
+            }
+        }
+    }
+    void Shuffle<T>(List<T> list)
+    {
+        int count = list.Count;
+
+        for (int i = 0; i < count; i++)
+        {
+            int j = Random.Range(i, list.Count);
+            var pickedNumber = list[i];
+            list[i] = list[j];
+            list[j] = pickedNumber;
+        }
+    }
 
     private IEnumerator GetWikipediaData(string url)
     {
@@ -39,32 +186,12 @@ public class Scraper : MonoBehaviour
             }
             else
             {
+                Debug.Log("Connected");
                 string htmlContent = webRequest.downloadHandler.text;
                 ParseHtml(htmlContent);
             }
         }
     }
-    List<string> allSongs = new List<string>();
-
-    List<T> ShuffledList<T>(List<T> list)
-    {
-        int count = list.Count;
-
-        for (int i = 0; i < count; i++)
-        {
-            int j = Random.Range(i, list.Count);
-            var pickedNumber = list[i];
-            list[i] = list[j];
-            list[j] = pickedNumber;
-        }
-        if (list.FindAll(x=> x == list.Contains(x))) 
-        {
-        
-        }
-        return list;
-    }
-    
-
 
 
     private void ParseHtml(string html)
@@ -72,21 +199,36 @@ public class Scraper : MonoBehaviour
         var htmlDoc = new HtmlDocument();
         htmlDoc.LoadHtml(html);
 
-        var nodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='mw-category-group']//ul/li/a");
+        var nodes = htmlDoc.DocumentNode.SelectNodes("//div[@class='mw-category-group']//ul/li/a | //div[@class='mw-category-group']//ul/li/span/a");
         if (nodes == null)
         {
             Debug.Log("No data found.");
             return;
         }
-        List<string> check = new List<string>(); 
+
+        HashSet<string> tempSongSet = new HashSet<string>();
         foreach (var node in nodes)
         {
-            string song = node.InnerText.Trim();
-            if(!check.Contains(song)) 
+            string song = HttpUtility.HtmlDecode(node.InnerText.Trim());
+            if (song.Contains("song"))
             {
-                check.Add(song);
+                string remover = "(song)";
+                if (song.Contains("(Rick Astley song)")) 
+                {
+                    remover = "(Rick Astley song)";
+                }
+                string replacedSong = song.Replace(remover, string.Empty);
+                tempSongSet.Add(replacedSong);
+            }
+            else 
+            {
+                tempSongSet.Add(song);
             }
         }
-        allSongs = check;
+
+        allSongsList = tempSongSet.ToList();
+        shuffledList = allSongsList;
+        isDataReady = true;
+        Debug.Log("Complete Data Binding");
     }
 }
